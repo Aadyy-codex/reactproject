0426fd5aca4634caa2a3edf8b1a2cfa08b522b28
sumitdevraj <preetam@dice-academy.com>
2025-02-15 17:45:38 +0530

    added grand total

--- src/component/Cart.jsx
+++ src/component/Cart.jsx
@@ -3,36 +3,40 @@ import { store2 } from '../store'
 
 const Cart = () => {
 
-    let {cart,setCart}= useContext(store2);
-    
-    console.log(cart,"cart");
-    const removeFromCart = (index) => {
-        setCart(cart.filter((_, i) => i!== index))
-    }
-    const IncQty = (newItem) =>{
-      const itemIndex = cart.findIndex(item => item.title === newItem.title);
-      if (itemIndex !== -1) {
-        // Product exists, so increase quantity
-        const updatedCart = [...cart];
-        updatedCart[itemIndex].Selquantity += 1;
-        setCart(updatedCart);
-    } 
+  let { cart, setCart } = useContext(store2);
 
+  console.log(cart, "cart");
+  const removeFromCart = (index) => {
+    setCart(cart.filter((_, i) => i !== index))
+  }
+  const IncQty = (newItem) => {
+    const itemIndex = cart.findIndex(item => item.title === newItem.title);
+    if (itemIndex !== -1) {
+      const updatedCart = [...cart];
+      updatedCart[itemIndex].Selquantity += 1;
+      setCart(updatedCart);
     }
-    const DecQty = (newItem,idx)=>{
-      const itemIndex = cart.findIndex(item => item.title === newItem.title);
-      if (itemIndex !== -1) {
-        // Product exists, so increase quantity
-        const updatedCart = [...cart];
-        updatedCart[itemIndex].Selquantity -= 1;
-        setCart(updatedCart);
-    } 
-    if(newItem.Selquantity === 0){
+
+  }
+  const DecQty = (newItem, idx) => {
+    const itemIndex = cart.findIndex(item => item.title === newItem.title);
+    if (itemIndex !== -1) {
+      // Product exists, so increase quantity
+      const updatedCart = [...cart];
+      updatedCart[itemIndex].Selquantity -= 1;
+      setCart(updatedCart);
+    }
+    if (newItem.Selquantity === 0) {
       removeFromCart(idx)
     }
 
-    }
+  }
+  const grandTotal = (cart) =>{
+    let total = cart.reduce((acc,val)=>acc + val.Selquantity*val.price,0)
+    return total.toFixed(2)
     
+  }
+
   return (
 
     <div className='w-[90vw] border bg-slate-50 ml-[70px] mb-3 mt-5'>
@@ -40,17 +44,19 @@ const Cart = () => {
         cart.map((item, index) => (
           <div key={index}>
             {item.title} - {item.price} - {item.Selquantity}QTY - {item.Selquantity * item.price}total
-            <button onClick={()=>IncQty(item,index)}>+</button>
-            {item.Selquantity}QTY 
-            <button onClick={()=>DecQty(item,index)}>-</button>
+            <button onClick={() => IncQty(item, index)}>+</button>
+            {item.Selquantity}QTY
+            <button onClick={() => DecQty(item, index)}>-</button>
             <button onClick={() => removeFromCart(index)}>Remove</button>
+            
           </div>
-        )) 
-      :
-      <h1>cart is empty</h1>
+        ))
+        :
+        <h1>cart is empty</h1>
       }
+      {cart && cart.length && <h1>grand total{grandTotal(cart)}</h1>}
 
-      <button onClick={()=>setCart([])} className='bg-red-600 w-[10vw] font-bold rounded-md border border-black'>clear all </button>
+      <button onClick={() => setCart([])} className='bg-red-600 w-[10vw] font-bold rounded-md border border-black'>clear all </button>
     </div>
   )
 }
