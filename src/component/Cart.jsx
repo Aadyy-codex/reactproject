09939b6fcd7ed9877836ec048d528508dcf3553d
Aadyy-codex <adityasaini2602@gmail.com>
2025-02-15 23:53:10 +0530

    tailwind

--- src/component/Cart.jsx
+++ src/component/Cart.jsx
@@ -39,24 +39,27 @@ const Cart = () => {
 
   return (
 
-    <div className='w-[90vw]  ml-[70px] mb-3 mt-5'>
+    <div className='w-[90vw] ml-[70px] mb-3 mt-5'>
       {cart && cart.length > 0 ?
         cart.map((item, index) => (
-          <div key={index}>
-            {item.title} - {item.price} - {item.Selquantity}QTY - {item.Selquantity * item.price}total
-            <button onClick={() => IncQty(item, index)}>+</button>
-            {item.Selquantity}QTY
-            <button onClick={() => DecQty(item, index)}>-</button>
-            <button onClick={() => removeFromCart(index)}>Remove</button>
-            
+          <div key={index} className='text-lg flex font-medium mb-2'>
+            <div className='flex-1'>
+              {item.title} - ${item.price} - x{item.Selquantity}
+            </div>
+            <div className='flex items-center'>
+              <button onClick={() => DecQty(item, index)} className='px-2 py-1 bg-red-500 text-white rounded-l'>-</button>
+              <span className='px-4'>{item.Selquantity}QTY</span>
+              <button onClick={() => IncQty(item, index)} className='px-2 py-1 bg-green-500 text-white rounded-r'>+</button>
+              <button onClick={() => removeFromCart(index)} className='ml-4 px-3 py-1 bg-yellow-500 text-black border border-black rounded'>Remove</button>
+            </div>
           </div>
         ))
         :
-        <h1>cart is empty</h1>
+        <h1 className='text-xl'>Cart is empty</h1>
       }
-      {cart && cart.length && <h1>grand total{grandTotal(cart)}</h1>}
+      {cart && cart.length > 0 && <h1 className='text-2xl font-semibold mt-4'>Grand total : ${grandTotal(cart)}</h1>}
 
-      <button onClick={() => setCart([])} className='bg-red-600 w-[10vw] font-bold rounded-md border border-black'>clear all </button>
+      <button onClick={() => setCart([])} className='bg-gray-600 w-[10vw] h-[5vh] font-bold rounded-md border border-black text-xl text-white mt-4'>Clear all</button>
     </div>
   )
 }
